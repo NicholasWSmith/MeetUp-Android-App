4baa7fdc785929314079a09a157e1a4c0517a520
Nicholas Smith <fireblade.nicholas@hotmail.com>
2015-11-28 15:03:02 -0600

    Inital Build

---
+++ MyApplication/app/src/main/java/com/example/nickwsmith/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.nickwsmith.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView firstTextView = (TextView) findViewById(R.id.textView);
+        Button firstButton = (Button) findViewById(R.id.logInButton);
+
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                firstTextView.setText("You Clicked");
+            }
+        });
+    }
+
+
+
+}
